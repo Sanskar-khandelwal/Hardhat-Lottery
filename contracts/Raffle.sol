549d1802522510d9d1eee0b5ee106604cf15721b
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2023-04-27 14:45:15 +0530

    added how to chainlink vrf to get a random number and also added the functionality to get a random winner

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,4 +1,4 @@
-// Raffle 
+// Raffle
 //Enter the lottery (paying some amount)
 
 // Pick a random winner(paying some amount)
@@ -7,59 +7,92 @@
 
 //chainlink oracle -> Randomness, Automated Execution (chainlink Keepers )
 
-
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol"
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughtEthEntered();
+error Raffle__TransferFailed();
 
-contract Raffle is VRFConsumerBaseV2{
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+
+    /* Lottery variables */
+    address private s_recentWinner;
 
     /*Events */
     event RaffleEnter(address indexed player);
-  
-  constructor(address vrfCoorinatorV2,uint256 entranceFee) VRFConsumerBaseV2(vrfCoorinatorV2){
-    i_entranceFee = entranceFee;
-    i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoorinatorV2)
-  }
-  function enterRaffle() public payable {
-    if(msg.value < i_entranceFee){
-        revert Raffle__NotEnoughtEthEntered();
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
+
+    constructor(
+        address vrfCoorinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoorinatorV2) {
+        i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoorinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
-    s_players.push(payable(msg.sender));
-    emit RaffleEnter(msg.sender);
 
-  }
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughtEthEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
 
-    
-  function requestRandomWinner() external {
-    
- i_vrfCoordinator.requestRandomWords(
-            keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords)
-  }
+    function requestRandomWinner() external {
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATION,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
 
-  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+    function fulfillRandomWords(
+        uint256 /*requestId*/,
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        //require(success)
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
 
-  }
+    /* view pure function */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
 
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
 
-/* view pure function */
-function getEntranceFee() public view returns(uint256){
-    return i_entranceFee;
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
+    }
 }
-
-function getPlayer(uint256 index) public view returns(address){
-    return s_players[index];
-} 
-} 
