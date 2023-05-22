991eda4fe4751c4b41623771f152141d760538f3
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2023-05-22 15:47:01 +0530

    added this file for helping mocks

---
+++ helper-hardhat-config.js
@@ -0,0 +1,13 @@
+const networkConfig = {
+    11155111: {
+        name: "sepolia",
+        vrfCordinatorV2: 0x8103b0a8a00be2ddc778e6e7eaa21791cd364625,
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
