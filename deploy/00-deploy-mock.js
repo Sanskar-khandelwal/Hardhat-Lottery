7b7c27403727e1995eccf0aab2c4c98a80fb577a
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2023-05-28 15:25:43 +0530

    added a deploy mock script

--- deploy/00-deploy-mock.js
+++ deploy/00-deploy-mock.js
@@ -1,7 +1,15 @@
-const { developmentChains } = require("../helper-hardhat-config")
+const { ethers } = require("ethers")
+
+const { developmentChains } = require("../helper-hardhat-config") //0.25 is the premium. It cost .25 link per request
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // .25 is the premium. It cost .25 link
+const GAS_PRICE_LINK = 1e9
+
+//calculated value based on the gas price of the chain.
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const args = [BASE_FEE, GAS_PRICE_LINK]
     const chainId = network.config.chainId
 
     if (developmentChains.includes(network.name)) {
@@ -10,6 +18,11 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         await deploy("VRFCoordinatorV2Mock", {
             from: deployer,
             log: true,
+            args: args,
         })
+        log("MOCKS DEPLOYED!")
+        log("----------------------------------")
     }
 }
+
+module.exports.tags = ["all", "mocks"]
